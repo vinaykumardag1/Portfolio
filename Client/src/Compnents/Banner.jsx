9dabece8ffdcc5e735ad9185d519732eb39f0a8d
dagguapti vinaykumar <112471376+vinaykumardag1@users.noreply.github.com>
2024-08-09 20:45:35 +0530

    Update Banner.jsx

--- Client/src/Compnents/Banner.jsx
+++ Client/src/Compnents/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className='flex flex-col-reverse md:grid grid-cols-2 md:px-10 py-5'>
       <div className='flex justify-center  align-bottom'>
-        <div className='w-2/3 pt-10'>
+        <div className='w-2/3 h-2/3 pt-10'>
         <p className='text-xl '>Hello ,</p>
         <h2 className='text-4xl font-bold py-3'>I AM VINAY</h2>
         <p className='py-2 texr-xl'>A detail-oriented fresher with strong foundation in frontend development, proficient in HTML, CSS, JavaScript , Bootstrap and React.js. Eager to contribute to a dynamic team, create visually appealing, user-friendly, and  responsive interfaces to drive organizational success.</p>
@@ -12,7 +12,7 @@ const Banner = () => {
         </div>
       </div>
       <div className='rounded h-2/3 flex justify-center'>
-        <img src={banner_image} alt="my image protrait image " className=" h-2/3 rounded-3xl overflow-hidden"  />
+        <img src={banner_image} alt="my image protrait image " className=" h-auto rounded-3xl overflow-hidden"  />
       </div>
     </div>
   )
